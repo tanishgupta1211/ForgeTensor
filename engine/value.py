8a168bf6d3097d2f9771371085d2a695a33406f5
Tanish Gupta <tanish00008@gmail.com>
2026-05-24 21:40:05 +0530

    Add computational graph tracking

--- engine/value.py
+++ engine/value.py
@@ -1,13 +1,21 @@
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self.prev = set(_children)
+        self.op = _op
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        return Value(self.data + other.data)
+        out = Value(
+            self.data + other.data,
+            (self, other),
+            '+'
+        )
+
+        return out
 
 
 a = Value(2.0)
@@ -16,3 +24,5 @@ b = Value(3.0)
 c = a + b
 
 print(c)
+print(c.prev)
+print(c.op)
